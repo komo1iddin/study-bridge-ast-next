93309f6c96013e1112b65823fed3da26fda240e7
Komoliddin <komo1iddin.pro@gmail.com>
2025-04-13 15:58:12 +0800

    Expand modular translation system with additional components and utilities

--- messages/index.js
+++ messages/index.js
@@ -3,20 +3,43 @@ import navbarEn from './components/navbar/en.json';
 import navbarRu from './components/navbar/ru.json';
 import navbarUz from './components/navbar/uz.json';
 
+import footerEn from './components/footer/en.json';
+import footerRu from './components/footer/ru.json';
+import footerUz from './components/footer/uz.json';
+
+import contactFormEn from './components/contact-form/en.json';
+// Placeholder for other languages - will be added as they're created
+// import contactFormRu from './components/contact-form/ru.json';
+// import contactFormUz from './components/contact-form/uz.json';
+
+// Import common translations
+import commonEn from './common/en.json';
+import commonRu from './common/ru.json';
+import commonUz from './common/uz.json';
+
 // Combine all translations
 export const messages = {
   en: {
     navbar: navbarEn,
+    footer: footerEn,
+    contactForm: contactFormEn,
+    common: commonEn,
     // home: homeEn,
     // Add other sections as they're created
   },
   ru: {
     navbar: navbarRu,
+    footer: footerRu,
+    contactForm: contactFormEn,
+    common: commonRu,
     // home: homeRu,
     // Add other sections as they're created
   },
   uz: {
     navbar: navbarUz,
+    footer: footerUz,
+    contactForm: contactFormEn,
+    common: commonUz,
     // home: homeUz,
     // Add other sections as they're created
   },
