a1bebfad0b0e169648554564a2fa71b08f00d28e
Komoliddin <komo1iddin.pro@gmail.com>
2025-04-14 22:22:56 +0800

    fix: exclude /admin path from middleware routing

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
 
 // Skip all paths that should not be internationalized
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)']
+  matcher: ['/((?!api|_next|admin|.*\\..*).*)']
 }
