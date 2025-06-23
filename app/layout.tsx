1166b421e5f540aed911614e08c53bbaca0bc441
Komoliddin <komo1iddin.pro@gmail.com>
2025-06-23 16:34:51 +0800

    chore: add font-sans class to html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${inter.variable} ${raleway.variable}`}>
+    <html lang="en" className={`${inter.variable} ${raleway.variable} font-sans`}>
       <head>
         <meta name="color-scheme" content="light" />
         <meta name="theme-color" content="#ffffff" />
